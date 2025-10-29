48c2753cd45292ccfcebb907664edee69f994dcc
Wanji Kay <cirukins@hotmail.com>
2025-10-28 20:15:52 -0400

    created appointments controller and created instances

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,11 @@
 class Appointment < ApplicationRecord
   belongs_to :user
+
+  validates :user_cannot_book_multiple, on: :update
+
+  def user_cannot_book_multiple
+    if booked && user.appointments.where(booked: true).exists?
+      errors.add(:base, "Honey, you can't sit in two chairs at once!")
+    end
+  end
 end
