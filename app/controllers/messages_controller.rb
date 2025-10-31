3c45673c745e467cf7885bed2de14639044bd1f8
Wanji Kay <cirukins@hotmail.com>
2025-10-30 20:47:25 -0400

    created appointments seed and updated chats controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @chat = Chat.find(params[:chat_id])
-    @message = @chat.messages
+    @messages = @chat.messages
   end
 
   def new
@@ -15,22 +15,26 @@ class MessagesController < ApplicationController
     @chat = Chat.find(params[:chat_id])
     @message = Message.new(role: "user", content: params[:message][:content], chat:@chat)
     if @message.save
-     @ruby_llm_chat = RubyLLM.chat
-    response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
-    Message.create(role: "assistant", content: response.content, chat: @chat)
-    redirect_to chat_messages_path(@chat)
-  else
-    render :new, status: :unprocessable_entity
+      @ruby_llm_chat = RubyLLM.chat
+      response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+      Message.create(role: "assistant", content: response.content, chat: @chat)
+      redirect_to chat_messages_path(@chat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-def chat_context
-  "Here is the context of the chat: #{@chat.content}."
+  def appointment_context
+    appointment = @chat.appointment
+    "Here is the context of the appointment: #{appointment.content}, #{appointment.time}, #{appointment.location}."
+  end
+
+  def instructions
+    [SYSTEM_PROMPT, appointment_context]
+    .compact.join("\n\n")
+  end
 end
 
-def instructions
-  [SYSTEM_PROMPT, challenge_context, @challenge.system_prompt]
-  .compact.join("\n\n")
-end
-end
+user= User.create!(email: "client@example.com", password: "password")
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,16 @@
 class Appointment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
-  validates :user_cannot_book_multiple, on: :update
+  validate :user_cannot_book_multiple, on: :update
+
   validates :time, presence: true
-  validates :location, precences: true
+  validates :location, presence: true
+  validates :stylist_Name, presence: true
+
+  private
 
   def user_cannot_book_multiple
-    if booked && user.appointments.where(booked: true).exists?
+    if booked && user && user.appointments.where(booked: true).where.not(id: id).exists?
       errors.add(:base, "Honey, you can't sit in two chairs at once!")
     end
   end
