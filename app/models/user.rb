71f5c5c959d69cd434b34f876d8455fc1c367326
WanjiKay <cirukins@hotmail.com>
2025-11-01 14:20:46 -0400

    Revert "created chat indec and chat new files"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :appointments, dependent: :nullify
-  has_many :chats
   has_one_attached :avatar
 
   def avatar_url
