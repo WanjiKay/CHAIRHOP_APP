f1e90be84517f72a49552be333066ef02ab68ba5
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2025-11-04 20:46:41 -0500

    fixed error,now chat can detect and describe images

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,5 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :appointment
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,20 @@ class Message < ApplicationRecord
   belongs_to :chat
   has_many_attached :photos
   validates :content, presence: true
+
+
+  validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
+  # validate :file_size_validation
+
+  MAX_FILE_SIZE_MB = 10
+
+  private
+
+  def file_size_validation
+    photos.each do |photo|
+      if photo.attached? && photo.byte_size > MAX_FILE_SIZE_MB.megabytes
+        errors.add(:photos, "size must be less than #{MAX_FILE_SIZE_MB}MB")
+      end
+    end
+  end
 end
