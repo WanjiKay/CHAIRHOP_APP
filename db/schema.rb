841ba0f592a7ef7905439e20c13ed69b0d374c7b
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2025-11-08 17:30:27 -0500

    added rouge

--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_08_183543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_tool_calls_on_message_id"
-    t.index ["tool_call_id"], name: "index_tool_calls_on_tool_call_id"
   end
 
   create_table "users", force: :cascade do |t|
