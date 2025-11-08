a336c609883aa1a00c23ba973af7ae2c1c924f3b
Wanji Kay <cirukins@hotmail.com>
2025-11-08 16:37:44 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,4 @@ Rails.application.routes.draw do
   resources :chats, only: [:index, :new, :create, :show] do
     resources :messages, only: [:index, :create]
   end
-  resources :chats, only: [:index, :show, :new, :create] do
-    resources :messages, only: [:create]
-  end
 end
